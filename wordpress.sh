d7d7d8bb15e70f46374ba065d962a7e6308b66ab
Gowtham <gurramgowtham1970@gmail.com>
2024-10-21 16:19:55 +0530

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo yum -y install git
-git clone https://github.com/Gowtham-200/wordpress-docker.git
 sudo yum -y install docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo chmod 666 /var/run/docker.sock
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+git clone https://github.com/Gowtham-200/wordpress-docker.git
+cd /
+sudo mv wordpress-docker/* /home/ec2-user/
 docker-compose up -d
