497f8518e6e75f5140f301d2f48ed3924e3603d9
Gowtham <gurramgowtham1970@gmail.com>
2024-11-28 16:30:13 +0530

    Update ec2-instance1.tf

--- ec2-instance1.tf
+++ ec2-instance1.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "wordpress-instance1" {
   ami                         = "ami-0e54eba7c51c234f6" # Amazon Linux 2 AMI
   instance_type               = "t2.micro"
   count                       = 1
-  key_name                    = "goutham"
+##  key_name                    = "goutham"
   vpc_security_group_ids      = [aws_security_group.Gowtham-sg.id]
   subnet_id                   = aws_subnet.web1-public-subnet.id
   associate_public_ip_address = true
